506c900501856ed993cf5290bc2f4ac132117a63
sebastian Tovar Chavez <sebastiantovarchavez304@gmail.com>
2023-01-16 16:12:27 -0500

    mi primer commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="bg-red-500">hola</h1>
+  </div>
+
+  
+</template>
+
+<style scoped>
+
+
+</style>
